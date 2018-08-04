44e81847412cc95814709213ba11004ae9f4d9e8
Erman Sahin Tatar <ermantatar@gmail.com>
2018-08-04 16:18:57 -0400

    Add files via upload

---
+++ MainTabBarController.swift
@@ -0,0 +1,52 @@
+//
+//  MainTabBarController.swift
+//  Podio
+//
+//  Created by Erman Sahin Tatar on 8/4/18.
+//  Copyright © 2018 Erman Sahin Tatar. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Navigation Bar Page Title make bigger for all.
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        setupViewControllers()
+    }
+    
+    //MARK:- Setup Functions
+    
+    func setupViewControllers() {
+        viewControllers = [
+            generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
+            generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
+            generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
+        ]
+    }
+    
+    //MARK:- Helper Functions
+    
+    fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        
+        let navController = UINavigationController(rootViewController: rootViewController)
+        // Make page title for all bar items.
+        rootViewController.navigationItem.title = title
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        return navController
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podio
+//
+//  Created by Erman Sahin Tatar on 8/4/18.
+//  Copyright © 2018 Erman Sahin Tatar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
+    }
+
+}
+
